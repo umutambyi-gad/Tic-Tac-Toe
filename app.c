1171aef1cf873ef16b750e6a6a955afdb7b65eff
Umutambyi Gad <umutambyig@gmail.com>
2021-03-08 20:14:11 +0100

    Defined function for displaying tic tac toe tittle

---
+++ app.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct Title {char* characters; } Title;
+
+void displayTitle();
+
+int main() {
+    displayTitle();
+    return 0;
+}
+
+void displayTitle() {
+    int i;
+    Title title[] = {
+        {"\n"},
+        {"_______ _        _______           _______ \n"},
+        {"|__   __(_)      |__   __|         |__   __| \n"},
+        {"   | |   _  ___     | | __ _  ___     | | ___   ___ \n"},
+        {"   | |  | |/ __|    | |/ _` |/ __|    | |/ _ \\ / _ \\ \n"},
+        {"   | |  | | (__     | | (_| | (__     | | (_) |  __/ \n"},
+        {"   |_|  |_|\\___|    |_|\\__,_|\\___|    |_|\\___/ \\___| \n\n\n"}
+    };
+    for (i = 0; i < sizeof(title) / sizeof(*title); i++) printf("%s", title[i].characters);
+}
